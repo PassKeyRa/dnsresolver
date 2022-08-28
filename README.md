1b2b7a2081b33e45929e793d8b10ebbec5430492
Alexey Posikera <posikera@bk.ru>
2022-08-28 19:11:45 +0300

    README updates, format and quiet options added

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # DNS resolver
 
-Multi-threaded DNS resolver to resolve ip addresses for a lot of domains
+Multi-threaded DNS resolver to resolve ip addresses for lots of domains
+
+## Usage
+
+You can set the output file, output format, number of threds and quiet parameters. In case the default format `%IP%,%DOMAIN%` is not suitable for you it's possible to set the custom format. Quiet option will enable the quiet mode, when info and status are not printed, only results.
+
+```
+usage: getip.py [-h] [-o OUTFILE] [-f FORMAT] [-t THREADS] [-q] domain_list
+
+DNS ip resolver
+
+positional arguments:
+  domain_list           File with domains
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTFILE, --outfile OUTFILE
+                        Output file. Print to stdout if not specified
+  -f FORMAT, --format FORMAT
+                        Specify output format (e.g. '%IP%,%DOMAIN%' (default) or '%DOMAIN%,%IP%')
+  -t THREADS, --threads THREADS
+                        Number of threads [default: 100]
+  -q, --quiet           Do not print anything except results
+```
+
+**Running example:**
+
+```bash
+python3 getip.py -o results.txt -t 50 domains.txt
+```
--- getip.py
+++ getip.py
@@ -6,17 +6,25 @@ import time
 
 from gevent import socket
 from gevent.pool import Pool
+from colorama import Fore, init
+init(autoreset=True)
 
 resolved = []
 num = 0
 out_of = 0
+
+quiet = False
+
+def print_q(s: str):
+    if not quiet:
+        print(s)
 
 def resolve(url):
     global resolved
     global num
     global out_of
     try:
-        print(f'Resolving domain {num} out of {out_of}')
+        print_q(Fore.YELLOW + f'[STATUS] Resolving domain {num} out of {out_of}')
         with gevent.Timeout(20):
             data = [socket.gethostbyname(url), url]
             resolved.append(data)
@@ -24,26 +32,32 @@ def resolve(url):
         pass
     num += 1
 
+
+
 def main():
     global num
     global resolved
     global out_of
+    global quiet
     parser = argparse.ArgumentParser(description='DNS ip resolver')
-    parser.add_argument('-o', '--outfile', default=None, help='Output file')
-    #parser.add_argument('-f', '--format', default='ip,domain', help='Specify output format (e.g. \'ip,domain\' or \'domain,ip\')')
-    parser.add_argument('-t', '--threads', type=int, default=100, help='Number of threads')
+    parser.add_argument('-o', '--outfile', default=None, help='Output file. Print to stdout if not specified')
+    parser.add_argument('-f', '--format', default='%IP%,%DOMAIN%', help='Specify output format (e.g. \'%%IP%%,%%DOMAIN%%\' (default) or \'%%DOMAIN%%,%%IP%%\')')
+    parser.add_argument('-t', '--threads', type=int, default=100, help='Number of threads [default: 100]')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='Do not print anything except results')
     parser.add_argument('domain_list', help='File with domains')
     args = parser.parse_args()
 
     ofile = args.outfile
+    quiet = args.quiet
+    format_ = args.format
     st = time.time()
     urls = []
-    print('Reading file...')
+    print_q(Fore.YELLOW + '[STATUS] Reading file...')
     with open(args.domain_list, 'r') as f:
         for line in f:
             urls.append(line.strip())
     tt = time.time() - st
-    print(f'Loaded data in {tt}')
+    print_q(Fore.YELLOW + f'[STATUS] Loaded data in {tt}')
     out_of = len(urls)
     
     st = time.time()
@@ -54,11 +68,23 @@ def main():
         pool.join()
     except KeyboardInterrupt:
         pass
+
+    output = ''
+    for i in range(len(resolved)):
+        ip = resolved[i][0]
+        domain = resolved[i][1]
+        output += format_.replace('%IP%', ip).replace('%DOMAIN%', domain)
+        if i != len(resolved) - 1:
+            output += '\n'
     if ofile:
         with open(ofile, 'w') as f:
-            f.write('\n'.join([','.join(i) for i in resolved]))
+            f.write(output)
+    else:
+        print_q('')
+        print(output)
+        print_q('')
     tt = time.time() - st
-    print(f'Resolved {len(resolved)} in {tt}')
+    print_q(Fore.GREEN + f'[INFO] Resolved {len(resolved)} in {tt}')
         
 
 if __name__ == '__main__':
